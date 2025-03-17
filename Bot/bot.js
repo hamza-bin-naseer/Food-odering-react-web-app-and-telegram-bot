20263d34943699451a5ee960e1382e2517b9d907
Hamza Bin Naseer <hamzabinnaseer23@.com>
2025-03-17 16:01:07 +0500

    Initial commit

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "7698901836:AAF_3XfsiihF73s3GOhH8vzzilG22myTHIU";
+const TOKEN = "";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://velvety-monstera-53938c.netlify.app/";
